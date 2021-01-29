4309735312d5b42062f8657a0b4b99c55ed7fb15
Vladislava Markova <vladislavam1996@gmail.com>
2021-01-29 09:32:33 +0200

    add some screenshots of the system and update seed and test_history in database_seeds

--- database_seeds/test_history.sql
+++ database_seeds/test_history.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 27, 2021 at 12:11 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.2.34
+-- Generation Time: Jan 29, 2021 at 08:29 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,6 +36,19 @@ CREATE TABLE `test_history` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `test_history`
+--
+
+INSERT INTO `test_history` (`id`, `user_id`, `topic_id`, `timestamp`, `score`) VALUES
+(1, 81271, 0, '2021-01-29 08:23:25', 1),
+(2, 81271, 0, '2021-01-29 08:23:50', 4),
+(3, 80995, 0, '2021-01-29 08:24:49', 5),
+(4, 80995, 0, '2021-01-29 08:25:02', 1),
+(5, 81319, 0, '2021-01-29 08:25:33', 5),
+(6, 84517, 0, '2021-01-29 08:26:48', 2),
+(7, 84517, 0, '2021-01-29 08:27:18', 0);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -53,7 +66,7 @@ ALTER TABLE `test_history`
 -- AUTO_INCREMENT for table `test_history`
 --
 ALTER TABLE `test_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
